b5ddb47b20f529f71d5895f9364da9999d0e2db9
Student <m.d.oroark@gmail.com>
2016-02-16 15:25:49 -0700

    functional form writes to data2.txt

---
+++ app.js
@@ -0,0 +1,54 @@
+// Requires \\
+var express = require('express');
+var bodyParser = require('body-parser');
+var logger = require('morgan');
+var fs = require('fs')
+// Create Express App Object \\
+var app = express();
+
+
+
+
+// Application Configuration \\
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(express.static(__dirname + '/public'));
+
+// Routes \\
+app.get('/:filename', function(req, res){
+  	fs.readFile('public/' + req.params.filename ,function(err, data){
+
+    // do something with data here
+  		res.header('content-type', 'text/html')
+  		res.send(data)
+  		console.log(data)
+	});
+
+});
+
+
+app.get('/', function(req, res){
+	fs.readFile('public/data3.html', function(err, data){
+			console.log(err)
+    // do something with data here
+  		res.header('content-type', 'text/html')
+  		res.send(data)
+  		console.log(data)
+
+	});
+});
+
+app.post('/formsubmit', function(req,res){
+	res.send('success')
+	console.log(req.body.email)
+	fs.writeFileSync('public/data2.txt',req.body.email)
+})
+
+// Creating Server and Listening for Connections \\
+var port = 3000
+app.listen(port, function(){
+  console.log('Server running on port ' + port);
+
+})
+
