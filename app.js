7b48099bc774504010451ceaad1f0e195f92b17b
Student <m.d.oroark@gmail.com>
2016-02-16 16:04:32 -0700

    last commit of the day

--- app.js
+++ app.js
@@ -16,9 +16,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(express.static(__dirname + '/public'));
 
 // Routes \\
-app.get('/:filename', function(req, res){
-  	fs.readFile('public/' + req.params.filename ,function(err, data){
 
+app.get('public/:filename', function(req, res){
+  	fs.readFile('public/' + req.params.filename ,function(err, data){
+  		if (err) throw err
     // do something with data here
   		res.header('content-type', 'text/html')
   		res.send(data)
@@ -26,7 +27,6 @@ app.get('/:filename', function(req, res){
 	});
 
 });
-
 
 app.get('/', function(req, res){
 	fs.readFile('public/data3.html', function(err, data){
@@ -34,21 +34,33 @@ app.get('/', function(req, res){
     // do something with data here
   		res.header('content-type', 'text/html')
   		res.send(data)
-  		console.log(data)
+  		// console.log(data)
 
 	});
 });
 
+app.get('/success', function(req, res){
+	fs.readFile('public/success.html', function(err, data){
+		if (err) throw err
+    // do something with data here
+  		res.header('Content-Type', 'text/html')
+  		res.send(data)
+  		console.log('success data',data)
+
+	});
+});
+
+
 app.post('/formsubmit', function(req,res){
-	res.send('success')
-	console.log(req.body.email)
+	res.redirect('/success')
+	console.log('form-submit', req.body.email)
 	fs.writeFileSync('public/data2.txt',req.body.email)
 })
+
 
 // Creating Server and Listening for Connections \\
 var port = 3000
 app.listen(port, function(){
   console.log('Server running on port ' + port);
-
 })
 
